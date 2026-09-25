35a14a9d74e5912034080b517bb9e5521a561199
Hactazia <hactazia@gmail.com>
2026-09-25 18:12:07 +0200

    refactor(finalik): migrate VRIKWeightParameter to the new IParameter
    Implements Name/Key/ValueType/Flags/Value. The setter now switches on the
    (group, type) tuple directly instead of nesting the group switch and the
    WeightType comparison, which is the same dispatch in one step.
    
    Also drops the unused Nox.CCK.Utils import, the parameter reorders its members
    and the missing trailing newline is added.

--- Runtime/VRIKWeightParameter.cs
+++ Runtime/VRIKWeightParameter.cs
@@ -2,7 +2,6 @@
 using Nox.Avatars.Parameters;
 using Nox.CCK;
 using Nox.CCK.Network;
-using Nox.CCK.Utils;
 using RootMotion.FinalIK;
 using UnityEngine;
 
@@ -13,13 +12,12 @@ namespace Nox.Avatars.FinalIK {
 	/// </summary>
 	public class VRIKWeightParameter : IParameter {
 		public enum WeightType { Position, Rotation }
-
-		private readonly string     _name;
-		private readonly VRIK       _rig;
-		private readonly HumanBodyBonesGroup _group;
-		private readonly WeightType _type;
-
 		public enum HumanBodyBonesGroup { Head, LeftArm, RightArm, LeftLeg, RightLeg }
+
+		private readonly string               _name;
+		private readonly VRIK                 _rig;
+		private readonly HumanBodyBonesGroup _group;
+		private readonly WeightType           _type;
 
 		public VRIKWeightParameter(string name, VRIK rig, HumanBodyBonesGroup group, WeightType type) {
 			_name  = name;
@@ -28,54 +26,79 @@ namespace Nox.Avatars.FinalIK {
 			_type  = type;
 		}
 
-		public string GetName() => _name;
-		public bool   IsValid() => _rig;
-		public int    GetKey()  => GetName().Hash();
+		public string Name 
+			=> _name;
 
-		public ParameterType  GetValueType() => ParameterType.Float;
-		public ParameterFlags GetFlags()     => ParameterFlags.OwnerEditable | ParameterFlags.OwnerSyncsToViewers;
+		public bool IsValid() 
+			=> _rig != null;
 
-		public object Get() {
-			if (!_rig) return 0f;
-			return (_group, _type) switch {
-				(HumanBodyBonesGroup.Head,     WeightType.Position) => _rig.solver.spine.positionWeight,
-				(HumanBodyBonesGroup.Head,     WeightType.Rotation) => _rig.solver.spine.rotationWeight,
-				(HumanBodyBonesGroup.LeftArm,  WeightType.Position) => _rig.solver.leftArm.positionWeight,
-				(HumanBodyBonesGroup.LeftArm,  WeightType.Rotation) => _rig.solver.leftArm.rotationWeight,
-				(HumanBodyBonesGroup.RightArm, WeightType.Position) => _rig.solver.rightArm.positionWeight,
-				(HumanBodyBonesGroup.RightArm, WeightType.Rotation) => _rig.solver.rightArm.rotationWeight,
-				(HumanBodyBonesGroup.LeftLeg,  WeightType.Position) => _rig.solver.leftLeg.positionWeight,
-				(HumanBodyBonesGroup.LeftLeg,  WeightType.Rotation) => _rig.solver.leftLeg.rotationWeight,
-				(HumanBodyBonesGroup.RightLeg, WeightType.Position) => _rig.solver.rightLeg.positionWeight,
-				(HumanBodyBonesGroup.RightLeg, WeightType.Rotation) => _rig.solver.rightLeg.rotationWeight,
-				_                                                    => 0f
-			};
-		}
+		public int Key 
+			=> Name.Hash();
 
-		public void Set(object value) {
-			if (!_rig) return;
-			var w = Mathf.Clamp01(value.ToFloat());
-			switch (_group) {
-				case HumanBodyBonesGroup.Head:
-					if (_type == WeightType.Position) _rig.solver.spine.positionWeight = w;
-					else                              _rig.solver.spine.rotationWeight = w;
-					break;
-				case HumanBodyBonesGroup.LeftArm:
-					if (_type == WeightType.Position) _rig.solver.leftArm.positionWeight = w;
-					else                              _rig.solver.leftArm.rotationWeight = w;
-					break;
-				case HumanBodyBonesGroup.RightArm:
-					if (_type == WeightType.Position) _rig.solver.rightArm.positionWeight = w;
-					else                              _rig.solver.rightArm.rotationWeight = w;
-					break;
-				case HumanBodyBonesGroup.LeftLeg:
-					if (_type == WeightType.Position) _rig.solver.leftLeg.positionWeight = w;
-					else                              _rig.solver.leftLeg.rotationWeight = w;
-					break;
-				case HumanBodyBonesGroup.RightLeg:
-					if (_type == WeightType.Position) _rig.solver.rightLeg.positionWeight = w;
-					else                              _rig.solver.rightLeg.rotationWeight = w;
-					break;
+		public ParameterType ValueType 
+			=> ParameterType.Float;
+
+		public ParameterFlags Flags 
+			=> ParameterFlags.OwnerEditable 
+				| ParameterFlags.OwnerSyncsToViewers;
+
+		public object Value {
+			get {
+				if (!_rig) return 0f;
+				return (_group, _type) switch {
+					(HumanBodyBonesGroup.Head,     WeightType.Position) => _rig.solver.spine.positionWeight,
+					(HumanBodyBonesGroup.Head,     WeightType.Rotation) => _rig.solver.spine.rotationWeight,
+					(HumanBodyBonesGroup.LeftArm,  WeightType.Position) => _rig.solver.leftArm.positionWeight,
+					(HumanBodyBonesGroup.LeftArm,  WeightType.Rotation) => _rig.solver.leftArm.rotationWeight,
+					(HumanBodyBonesGroup.RightArm, WeightType.Position) => _rig.solver.rightArm.positionWeight,
+					(HumanBodyBonesGroup.RightArm, WeightType.Rotation) => _rig.solver.rightArm.rotationWeight,
+					(HumanBodyBonesGroup.LeftLeg,  WeightType.Position) => _rig.solver.leftLeg.positionWeight,
+					(HumanBodyBonesGroup.LeftLeg,  WeightType.Rotation) => _rig.solver.leftLeg.rotationWeight,
+					(HumanBodyBonesGroup.RightLeg, WeightType.Position) => _rig.solver.rightLeg.positionWeight,
+					(HumanBodyBonesGroup.RightLeg, WeightType.Rotation) => _rig.solver.rightLeg.rotationWeight,
+					_                                                    => 0f
+				};
+			}
+			set {
+				if (!_rig) return;
+				var w = Mathf.Clamp01(value.ToFloat());
+
+				switch (_group, _type) {
+					case (HumanBodyBonesGroup.Head, WeightType.Position):
+						_rig.solver.spine.positionWeight = w;
+						break;
+					case (HumanBodyBonesGroup.Head, WeightType.Rotation):
+						_rig.solver.spine.rotationWeight = w;
+						break;
+
+					case (HumanBodyBonesGroup.LeftArm, WeightType.Position):
+						_rig.solver.leftArm.positionWeight = w;
+						break;
+					case (HumanBodyBonesGroup.LeftArm, WeightType.Rotation):
+						_rig.solver.leftArm.rotationWeight = w;
+						break;
+
+					case (HumanBodyBonesGroup.RightArm, WeightType.Position):
+						_rig.solver.rightArm.positionWeight = w;
+						break;
+					case (HumanBodyBonesGroup.RightArm, WeightType.Rotation):
+						_rig.solver.rightArm.rotationWeight = w;
+						break;
+
+					case (HumanBodyBonesGroup.LeftLeg, WeightType.Position):
+						_rig.solver.leftLeg.positionWeight = w;
+						break;
+					case (HumanBodyBonesGroup.LeftLeg, WeightType.Rotation):
+						_rig.solver.leftLeg.rotationWeight = w;
+						break;
+
+					case (HumanBodyBonesGroup.RightLeg, WeightType.Position):
+						_rig.solver.rightLeg.positionWeight = w;
+						break;
+					case (HumanBodyBonesGroup.RightLeg, WeightType.Rotation):
+						_rig.solver.rightLeg.rotationWeight = w;
+						break;
+				}
 			}
 		}
 	}
